79c2a9452a7c8b7833c41e7537d967efe794e05d
Syed-Aqib-555 <syedaqibh555@gmail.com>
2026-05-08 01:48:48 +0500

    Add TodoService with create delete and toggle functionality

---
+++ TodoService.cs
@@ -0,0 +1,44 @@
+﻿using FocusFlow.Models;
+
+namespace FocusFlow.Services
+{
+    public class TodoService
+    {
+        private readonly List<TodoTask> tasks = new();
+
+        private int nextId = 1;
+
+        public List<TodoTask> GetTasks()
+        {
+            return tasks;
+        }
+
+        public void AddTask(TodoTask task)
+        {
+            task.Id = nextId++;
+            task.CreatedAt = DateTime.Now;
+
+            tasks.Add(task);
+        }
+
+        public void DeleteTask(int id)
+        {
+            var task = tasks.FirstOrDefault(x => x.Id == id);
+
+            if (task != null)
+            {
+                tasks.Remove(task);
+            }
+        }
+
+        public void ToggleTask(int id)
+        {
+            var task = tasks.FirstOrDefault(x => x.Id == id);
+
+            if (task != null)
+            {
+                task.IsCompleted = !task.IsCompleted;
+            }
+        }
+    }
+}
